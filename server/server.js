971522fb94b6dad86427c5a372cc871618bc5f92
Gavin Reid <gavinreid94@gmail.com>
2021-10-06 15:35:36 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require("./config/db");
+const productRoutes = require('./routes/productRoutes');
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 
