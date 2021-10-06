2c6d17820f163a54ac877a2096e87afd8987ffd8
Gavin Reid <gavinreid94@gmail.com>
2021-10-05 19:19:15 -0500

    Backend folder structure and server setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
