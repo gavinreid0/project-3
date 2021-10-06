ef5b482dcb1b11346f68c2de1f0677b1fac7cbac
isipcasinillo <isipcasinillo@gmail.com>
2021-10-05 20:23:31 -0500

    added schemas and model

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    location: String
+  }
+
+`;
+
+module.exports = typeDefs;
