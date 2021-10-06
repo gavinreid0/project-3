d6af6b60b7ef92c7b13630ca2c4f71ef71ea55ec
Gavin Reid <gavinreid94@gmail.com>
2021-10-06 12:21:34 -0500

    fixed broken code

--- server/schemas/resolvers.js
+++
@@ -1,9 +0,0 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
-const { signToken } = require('../utils/auth');
-
-const resolvers = {
-  
-};
-
-module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
