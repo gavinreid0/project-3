735a28beaea082c5347f56f2c698aa2112f249c6
Gavin Reid <gavinreid94@gmail.com>
2021-10-06 15:35:55 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
