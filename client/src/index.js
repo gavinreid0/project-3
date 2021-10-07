060fd6f9e9daca98f45ca2be89686c9556270044
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-10-06 19:33:15 -0700

    updating a basic layout on the client side/src folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.querySelector("#root")
 );
-
-reportWebVitals();
