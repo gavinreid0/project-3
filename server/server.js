dc29747ab7ac66b64b99ab1230ee7b9242792f6a
Gavin Reid <gavinreid94@gmail.com>
2021-10-06 20:25:24 -0500

    minor changes and changed data file to seeder

--- server/server.js
+++ server/server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes)
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
