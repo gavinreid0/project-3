663584188724c7a27632cbbfea8ddd49caba5191
Jose Rosas <josejrrosas@yahoo.com>
2021-10-09 10:34:35 -0500

    new template

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header";
-
-export { default as Product } from "./products/Product";
-export { default as ProductIndex } from "./products/ProductIndex";
-export { default as ProductDetails } from "./products/ProductDetails";
